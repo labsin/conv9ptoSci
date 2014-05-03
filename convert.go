8e31750b7c88caa913e83a434cdd27c6f8679ece
Sam Segers <sam.sgrs@gmail.com>
2014-05-03 21:20:51 +0200

    Cleanup

--- convert.go
+++ convert.go
@@ -26,66 +26,66 @@ func (self Sci) String() string {
 }
 
 func main() {
-	var sImg string
+	var imgFlag string
 	var verbose bool
-	flag.StringVar(&sImg, "img", "", "The 9patch image")
+	flag.StringVar(&imgFlag, "img", "", "The 9patch image")
 	flag.BoolVar(&verbose, "v", false, "Verbose output")
 	flag.Parse()
 
-	sPathImg := filepath.Dir(sImg)
-	sBaseImg := filepath.Base(sImg)
+	imgDir := filepath.Dir(imgFlag)
+	imgBase := filepath.Base(imgFlag)
 
-	if !strings.HasSuffix(sBaseImg, ".9.png") {
+	if !strings.HasSuffix(imgBase, ".9.png") {
 		log.Fatal("Provided image doesn't have .9.png extention")
 	}
 
-	sBaseNoExtImg := sBaseImg[0 : len(sBaseImg)-6]
+	imgNoExtBase := imgBase[0 : len(imgBase)-6]
 	if verbose {
-		fmt.Print("Image Path: " + sPathImg + " Base " + sBaseImg + "\n")
+		fmt.Print("Image Path: " + imgDir + " Base " + imgBase + "\n")
 	}
 
-	reader, err := os.Open(sImg)
+	fi, err := os.Open(imgFlag)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer reader.Close()
+	defer fi.Close()
 
-	m, _, err := image.Decode(reader)
+	img, _, err := image.Decode(fi)
 	if err != nil {
 		log.Fatal(err)
 	}
-	reader.Close()
+	fi.Close()
 
-	bounds := m.Bounds()
-	sci := Sci{-1, -1, -1, -1, sBaseNoExtImg + ".png"}
+	bounds := img.Bounds()
+	sci := Sci{-1, -1, -1, -1, imgNoExtBase + ".png"}
 	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
-		r, g, b, a := m.At(0, y).RGBA()
+		r, g, b, a := img.At(0, y).RGBA()
 		if verbose {
 			fmt.Printf("At y %d -> %d,%d,%d,%d\n", y, r, g, b, a)
 		}
-		if !(a == 0 || a == 65535) || !(r == 0 && g == 0 && b == 0) {
+		if !(a == 0 || a == 65535) || !(r == 0 && g == 0 && b == 0) { // When not black or transparant it's not a valid 9patch
 			log.Fatal("Not a valid 9patch file. Wrong pixel at (x,y): (0," + strconv.Itoa(y) + ")")
 		}
-		if sci.Top == -1 && a == 65535 {
+		if sci.Top == -1 && a == 65535 { // Wait for black pixel
 			sci.Top = y - 1
 		}
-		if sci.Top != -1 && a == 0 {
+		if sci.Top != -1 && a == 0 { // Wait for transparent pixel
 			sci.Bottom = bounds.Max.Y - y - 1
 			break
 		}
 	}
 	for x := bounds.Min.X; x < bounds.Max.X; x++ {
-		r, g, b, a := m.At(x, 0).RGBA()
+		r, g, b, a := img.At(x, 0).RGBA()
 		if verbose {
 			fmt.Printf("At x %d -> %d,%d,%d,%d\n", x, r, g, b, a)
 		}
-		if !(a == 0 || a == 65535) || !(r == 0 && g == 0 && b == 0) {
+		if !(a == 0 || a == 65535) || !(r == 0 && g == 0 && b == 0) { // When not black or transparant it's not a valid 9patch
 			log.Fatal("Not a valid 9patch file. Wrong pixel at (x,y): (" + strconv.Itoa(x) + ",0)")
 		}
-		if sci.Left == -1 && a == 65535 {
+		if sci.Left == -1 && a == 65535 { // Wait for black pixel
 			sci.Left = x - 1
 		}
-		if sci.Left != -1 && a == 0 {
+		if sci.Left != -1 && a == 0 { // Wait for transparent pixel
 			sci.Right = bounds.Max.X - x - 1
 			break
 		}
@@ -94,25 +94,25 @@ func main() {
 		fmt.Print(sci, "\n")
 	}
 
-	newM := m.(interface {
+	newImg := img.(interface {
 		SubImage(r image.Rectangle) image.Image
 	}).SubImage(image.Rect(1, 1, bounds.Max.X-1, bounds.Max.Y-1))
 
-	writer, err := os.Create(filepath.Join(sPathImg, sci.Source))
+	writer, err := os.Create(filepath.Join(imgDir, sci.Source))
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer writer.Close()
 
-	err = png.Encode(writer, newM)
+	err = png.Encode(writer, newImg)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Print("Wrote " + sci.Source + " to " + sPathImg + "\n")
+	fmt.Print("Wrote " + sci.Source + " to " + imgDir + "\n")
 	writer.Close()
 
-	sciBaseName := sBaseNoExtImg + ".sci"
-	sciWriter, err := os.Create(filepath.Join(sPathImg, sciBaseName))
+	sciBaseName := imgNoExtBase + ".sci"
+	sciWriter, err := os.Create(filepath.Join(imgDir, sciBaseName))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -123,5 +123,5 @@ func main() {
 	if err := sciWriter.Close(); err != nil {
 		log.Fatal(err)
 	}
-	fmt.Print("Wrote " + sciBaseName + " to " + sPathImg + "\n")
+	fmt.Print("Wrote " + sciBaseName + " to " + imgDir + "\n")
 }
